980df554c03e833d283e44e47aa443b6115e5f1d
Leo Liu <karta1041337@gmail.com>
2019-03-26 23:50:57 +0800

    Add Render in Node and Tree

--- render.go
+++ render.go
@@ -1,6 +1,9 @@
 package tree
 
-import "bytes"
+import (
+	"bytes"
+	"errors"
+)
 
 // Style define the outlook of the tree
 type Style struct {
@@ -8,6 +11,71 @@ type Style struct {
 	Line  string
 	End   string
 	Space string
+}
+
+func renderNode(buf *bytes.Buffer, node *Node, style *Style) error {
+	for _, v := range node.prefix {
+		switch v {
+		case 0:
+			buf.WriteString(style.Node)
+		case 1:
+			buf.WriteString(style.Line)
+		case 2:
+			buf.WriteString(style.End)
+		case 3:
+			buf.WriteString(style.Space)
+		default:
+			return errors.New("Invalid prefix")
+		}
+	}
+
+	buf.WriteString(node.content)
+	buf.WriteRune('\n')
+
+	return nil
+}
+
+func renderNodeList(buf *bytes.Buffer, list []*Node, style *Style) error {
+	for _, v := range list {
+		str, err := v.Render(style)
+		if err != nil {
+			return err
+		}
+		buf.WriteString(str)
+	}
+	return nil
+}
+
+// Render generate the node structure in string with given style
+func (n *Node) Render(style *Style) (string, error) {
+	if style == nil {
+		return "", errors.New("No style found")
+	}
+
+	buf := &bytes.Buffer{}
+
+	if err := renderNode(buf, n, style); err != nil {
+		return "", err
+	} else if err := renderNodeList(buf, n.children, style); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
+
+// Render generate the tree structure in string with its style
+func (t *Tree) Render() (string, error) {
+	if t.Style == nil {
+		return "", errors.New("No style found")
+	}
+
+	buf := &bytes.Buffer{}
+
+	if err := renderNodeList(buf, t.roots, t.Style); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
 
 func (t *Tree) String() string {
